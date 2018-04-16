89e024b2a42068ef892bee4e1904bf19cec32a07
Dan Studdert <danieljstuddert@gmail.com>
2018-04-16 12:23:56 +1000

    Chunk prefab outlines

---
+++ SuicideBurn/Assets/Scripts/Chunks/ChunkOutline.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkOutline : MonoBehaviour {
+    [Header("Border")]
+    public Vector3 borderSize;
+    public Color borderColour;
+
+    [Header("Lanes")]
+    public Vector3 laneSize;
+    public List<Vector3> lanePositions;
+    public Color laneColour;
+    public bool showLanes;
+
+    [Header("Obstacles")]
+    public Vector3 obstacleArea;
+    public Color obstacleColour;
+    public bool showObstacleArea;
+
+    private void OnDrawGizmos() {
+        Gizmos.color = borderColour;
+        Gizmos.DrawWireCube(transform.position, borderSize);
+    }
+
+    private void OnDrawGizmosSelected() {
+        if (showLanes) {
+            Gizmos.color = laneColour;
+
+            for (int i = 0; i < lanePositions.Count; i++) {
+                Vector3 position = transform.position + lanePositions[i];
+                Gizmos.DrawWireCube(position, laneSize);
+            }
+        }
+
+        if (showObstacleArea) {
+            Gizmos.color = obstacleColour;
+            Gizmos.DrawWireCube(transform.position, obstacleArea);
+        }
+    }
+}
