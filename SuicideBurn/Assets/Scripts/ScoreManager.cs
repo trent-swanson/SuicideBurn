62ce73159a9e6327dec46582b5f535d3477fd999
AIE\s170998 <eljosho1998@gmail.com>
2018-04-16 16:03:00 +1000

    Updated PlayerController and ScoreManager scripts. Added a function to the former that adds points if the player makes contact with a coin/collectible. Should work in theory.

--- SuicideBurn/Assets/Scripts/ScoreManager.cs
+++ SuicideBurn/Assets/Scripts/ScoreManager.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour {
 
+    public float playerScore;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        playerScore = 0.0f;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
