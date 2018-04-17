daafa20feec5f69d47b3e4b9b55766b4ceb8f84a
NerbzHub <nathan.nette@gmail.com>
2018-04-17 13:47:00 +1000

    Created prefabs for speedholder, shufflebag and chunk destroyer. Deleted object pool code Added ground stopping.

---
+++ SuicideBurn/Assets/Scripts/PlayerLanding.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLanding : MonoBehaviour
+{
+    private bool hasFound = false;
+    private float currentSpeed;
+    private GameObject speedHolder;
+    // Use this for initialization
+    void Start()
+    {
+        //speedHolder = GameObject.Find("SpeedHolder(Clone)");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //currentSpeed = speedHolder.GetComponent<SpeedHolder>().GetCurrentSpeed();
+        currentSpeed = GameObject.Find("SpeedHolder(Clone)").GetComponent<SpeedHolder>().GetCurrentSpeed();
+        //Debug.Log(currentSpeed);
+    }
+    
+}
---
+++ SuicideBurn/Assets/Scripts/SpeedHolder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedHolder : MonoBehaviour
+{
+    float speed = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = gameObject.GetComponent<SceneMover>().currentSpeed;
+        //if(gameObject.transform.position.y >= 10)
+        //{
+        //    gameObject.transform.SetPositionAndRotation(new Vector3(0, 0, -30), Quaternion.identity);
+        //}
+    }
+
+    public float GetCurrentSpeed()
+    {
+        return speed;
+    }
+}
