066df76888e6ef3105a80c9451cedb29e179f107
Dan Studdert <danieljstuddert@gmail.com>
2018-04-18 11:32:42 +1000

    Move background

---
+++ SuicideBurn/Assets/Scripts/BackgroundLerp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundLerp : MonoBehaviour {
+    public float moveSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(0, moveSpeed * Time.deltaTime, 0);
+	}
+}
