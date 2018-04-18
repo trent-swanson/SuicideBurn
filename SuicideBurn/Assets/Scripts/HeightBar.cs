ff442899112db5b42f7cd653b0f741990fbf84d7
AIE\s162866 <contact@trentwilliamswanson.com>
2018-04-18 11:11:02 +1000

    Added new background Fixed Hieght Bar

--- SuicideBurn/Assets/Scripts/HeightBar.cs
+++ SuicideBurn/Assets/Scripts/HeightBar.cs
@@ -8,27 +8,26 @@ public class HeightBar : MonoBehaviour {
     public float lerpTime;
     public AnimationCurve lerpCurve;
 
-    private float currentPerc;
     private ShuffleBag shuffleBag;
 
     private void Awake() {
         shuffleBag = FindObjectOfType<ShuffleBag>();
-        currentPerc = heightSlider.value;
     }
 
     public void AdjustSlider() {
+        StopAllCoroutines();
         StartCoroutine(MoveSlider((float)(shuffleBag.chunksCreated - 4) / (shuffleBag.maxChunksBeforeGround - 4)));
     }
 
     private IEnumerator MoveSlider(float finalPerc) {
         float t = 0f;
 
+        float currentPerc = heightSlider.value;
+
         while (t < lerpTime) {
             t += Time.deltaTime;
             heightSlider.value = Mathf.Lerp(currentPerc, finalPerc, lerpCurve.Evaluate(t / lerpTime));
             yield return null;
         }
-
-        currentPerc = finalPerc;
     }
 }
